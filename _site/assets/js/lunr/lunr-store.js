f6084bb19204db6749de3596eb6e633879ac30d7
jma1 <jason.ma@activenetwork.com>
2020-03-11 10:03:48 +0800

    change to jekyll

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2020/03/04/welcome-to-jekyll.html",
+        "teaser":null},{
+        "title": "什么是微服务",
+        "excerpt":"什么是微服务 微服务也被称为微服务架构，通常我们可以把我们的单体应用拆分为不同的模块，虽然在业界对于微服务还没有统一的定义， 但我们可以总结出， 这些模块具有以下的特点：     可以单独部署和测试   模块之间低耦合   可以快速开发   模块对应一个领域模型   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/03/05/what-is-microservice.html",
+        "teaser":null}]
